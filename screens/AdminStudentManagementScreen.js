6c11aa1c37d8b5ecf4d9b742e0b05f738b294d0d
TheMathGenius01 <parth.zanwar01@gmail.com>
2025-08-08 13:37:55 -0500

    Update AdminStudentManagementScreen.js

--- screens/AdminStudentManagementScreen.js
+++ screens/AdminStudentManagementScreen.js
@@ -196,15 +196,10 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
   modalOverlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0,0,0,0.4)',
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
     justifyContent: 'center',
     alignItems: 'center',
-    zIndex: 9999,
   },
   modalContent: {
     backgroundColor: '#1a365d',
